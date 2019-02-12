558c3ca370ddabf0b2cabcf0440691cc7d0ca01d
Mike Doig <mike.j.doig@gmail.com>
2019-02-12 03:57:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function happyHolidayTo(holiday, name) {
   return (`Happy ${holiday}, ${name}!`)
 }
 
-// function holidaysCountdown() {
+function holidaysCountdown() {
   
-//   return (`Happy holidays, ${name}!`)
-// }
+  return (`Happy holidays, ${name}!`)
+}
